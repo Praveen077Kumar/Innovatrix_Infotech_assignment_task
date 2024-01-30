5ce607226534173109d2820779dcae4e2cd7da5c
Praveen077Kumar <pk077prince@gmail.com>
2024-01-30 13:14:48 +0530

    changed vite config api proxy route, and configure the app.jsx components rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter,Routes,Route} from "react-router-dom";
 import Header from './components/Header'
-
-
+import Signin from "./pages/Signin";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header/>
+      <Routes>
+        <Route path="/signin" element={<Signin/>}></Route>
+      </Routes>
     </BrowserRouter>
   )
 }
