75055477fa825d1a732806c7e1cac39bb17ce2aa
Praveen077Kumar <pk077prince@gmail.com>
2024-01-30 15:52:44 +0530

    create middleware function for varify the user through token

---
+++ api/utils/userVarify.js
@@ -0,0 +1,15 @@
+import errorHandling from "./errorHandling";
+import jwt from "jsonwebtoken";
+
+export const varifyAccessToken = (req, res, next) => {
+  const SECRET_KEY = "testingSecretKeyForJWT";
+  const token = req.cookies.access_token;
+
+  if (!token) return next(errorHandling(401, "Unauthorized User!"));
+
+  jwt.verify(token, SECRET_KEY, (error, user) => {
+    if (error) return errorHandling(403, " Forbidden !");
+    req.user = user;
+    next();
+  });
+};
